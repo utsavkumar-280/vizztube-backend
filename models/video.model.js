2c37a566d6af332ec6e773f673bd895c4dd4f918
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-01 20:15:22 +0530

    feat: added video model

---
+++ models/video.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const VideoSchema = new Schema({
+	title: {
+		type: String,
+		required: "Title of the video is required",
+	},
+	vidURL: {
+		type: String,
+		required: "vidURL of the video is required",
+	},
+	category: {
+		type: String,
+		required: "Category of the video is required",
+	},
+	thumbnail: {
+		type: String,
+	},
+	author: {
+		type: String,
+	},
+});
+
+const Video = model("Video", VideoSchema);
+
+module.exports = { Video };
