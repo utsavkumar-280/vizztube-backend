9188e790413b8ea24e86aca00cd41b6c5b6a2138
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-01 20:13:47 +0530

    feat: added index.js file

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const dbConnection = require("./db/dbConnect.js");
+const route404Handler = require("./middlewares/route404Handler");
+const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(cors());
+
+dbConnection();
+
+app.get("/", (req, res) => {
+	res.send("Welcome to VizzTube Apis");
+});
+app.get("/hello", (req, res) => {
+	res.json({
+		success: true,
+		message: "Hey, What's up?",
+	});
+});
+
+app.use(route404Handler);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+	console.log(`server running on http://localhost:${PORT}`);
+});
