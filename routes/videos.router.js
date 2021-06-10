b259fca0374ae72aff50626ef0b872546c96f9d5
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-11 02:17:10 +0530

    feat: added a new post route

--- routes/videos.router.js
+++ routes/videos.router.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const { get } = require("lodash");
 const router = express.Router();
 
 const {
 	getAllVideos,
 	addNewVideo,
+	getVideoById,
 } = require("../controllers/videos.controller");
 
 router.route("/").get(getAllVideos).post(addNewVideo);
 
+router.route("/:vidId").get(getVideoById);
 module.exports = router;
