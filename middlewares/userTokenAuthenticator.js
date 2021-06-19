0fb6470d112bd46dde31054dd2ad68a993e52ca0
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-20 00:57:42 +0530

    feat: added user token authentication middleware

---
+++ middlewares/userTokenAuthenticator.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const userTokenAuthenticator = async (req, res, next) => {
+	try {
+		const tokenContainer = req.headers.authorization;
+		const token = tokenContainer.split(" ")[1];
+		const decoded = jwt.verify(token, JWT_SECRET);
+
+		const user = await User.findById(decoded.userId);
+
+		if (user) {
+			req.user = user;
+			next();
+		} else {
+			res.status(401).json({ message: "Unauthorized Access" });
+			return;
+		}
+	} catch (error) {
+		console.log(error);
+		res
+			.status(401)
+			.json({ message: "Unauthorized Access", errorMessage: error.message });
+	}
+};
+
+module.exports = userTokenAuthenticator;
